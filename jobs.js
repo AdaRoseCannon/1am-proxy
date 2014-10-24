3a0034dc6a88f04e35945f8c3dfcf05f9a548f1c
Ada Rose Edwards <ada@ada.is>
2014-10-24 14:45:42 +0000

    Redirect root name urls not ending in a /

--- jobs.js
+++ jobs.js
@@ -1,12 +1,18 @@
 'use strict';
 
 module.exports = [{
-	pattern: "^https://(www\\.)?1am\\.club/~([a-z1-9]+)/?(.*)",
+	pattern: "^https://(www\\.)?1am\\.club/~([a-z1-9]+)/(.*)",
 	type: "static",
 	target: "/home/{{2}}/public_html/",
 	rewriteURL: "/{{3}}",
-	comment: "Users public_html dir",
-	https: true
+	https: true,
+	comment: "Users public_html dir"
+},{
+	pattern: "^https://(www\\.)?1am\\.club/~([a-z1-9]+)$",
+	type: "redirect",
+	target: "https://{{1}}1am.club/~{{2}}/",
+	https: true,
+	comment: "Redirect ~ada to ~ada/"
 },{
 	pattern: "^http://(.*)",
 	type: "redirect",
@@ -15,9 +21,10 @@ module.exports = [{
 },{
 	type: "self-update",
 	deploy: {
-			watch: "https://github.com/AdaRoseEdwards/1am-proxy",
-			ref: "refs/heads/master",
-			run: "npm install",
-			folder: __dirname
-	}
+		watch: "https://github.com/AdaRoseEdwards/1am-proxy",
+		ref: "refs/heads/master",
+		run: "npm install",
+		folder: __dirname
+	},
+	comment: "Update the app when a push is recieved to master"
 }];
