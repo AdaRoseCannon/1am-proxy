048be03fce2abc3a945f8e52ba743e0642e7c3dc
Ada Rose Edwards <ada@ada.is>
2014-10-27 05:04:58 +0000

    Update display

--- website/index.js
+++ website/index.js
@@ -1,20 +1,35 @@
 'use strict';
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 
 // Express Middleware
 var compression = require('compression');
 var bodyParser = require('body-parser');
 var mustacheEngine = require('hogan-express');
 var cookieParser = require('cookie-parser');
+var marked = require('marked');
 
 /**
  * Local Variables
  */
 
+marked.setOptions({
+	gfm: true,
+	tables: true,
+	breaks: true,
+	pedantic: false,
+	sanitize: true,
+	smartLists: true,
+	smartypants: false
+});
+
 var app = express();
 var templateFolder = path.normalize(path.join(__dirname, './templates'));
 var staticFolder = path.normalize(path.join(__dirname, './static')); 
+var codeOfConduct = marked(fs.readFileSync(path.join(__dirname, '../coc.md'), {encoding: 'utf8'})); 
+var readMe = marked(fs.readFileSync(path.join(__dirname, '../README.md'), {encoding: 'utf8'}));
+
 
 app.engine('ms', mustacheEngine);
 app.set('view engine', 'ms');
@@ -29,8 +44,9 @@ app.use(compression({
 app.get('/', function (req, res) {
 	res.render('page', {
 		title: "1am Club",
+		body: readMe,
 		partials: {
-			header: 'header'
+			header: 'header',
 		}
 	});
 });
