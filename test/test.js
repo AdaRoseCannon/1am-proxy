a8cd84c1e7436316569ea33a3a69795a3c3382e8
Ada Rose Edwards <ada@ada.is>
2014-11-02 09:54:59 -0800

    Add error

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 var compile = require('../lib/compiler');
 var path = require('path');
-var log = function () {
-	console.log(arguments);
+var log = function (string) {
+	return function (message) {console.log(string || '', message);};
 };
-compile(path.join(__dirname, 'testFile.js')).then(log).catch(log);
+compile(path.join(__dirname, 'testFile.js')).then(log('Result:')).catch(log('Error:'));
