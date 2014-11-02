08423cffa8ba188394b0cbd68dc97530dd960f4b
Ada Rose Edwards <ada@ada.is>
2014-11-02 11:00:26 -0800

    Get pipes working

--- test/test.js
+++ test/test.js
@@ -1,7 +1,37 @@
 'use strict';
+/* global Promise */
 var compile = require('../lib/compiler');
+
 var path = require('path');
 var log = function (string) {
 	return function (message) {console.log(string || '', message);};
 };
-compile(path.join(__dirname, 'testFile.js')).then(log('Result:')).catch(log('Error:'));
+var testFile = path.join(__dirname, 'testFile.js');
+var t;
+t=Date.now();
+Promise.resolve(testFile)
+.then(compile)
+.then(function () {
+	console.log(Date.now() - t);
+	t=Date.now();
+	return testFile;
+})
+.then(compile)
+.then(function () {
+	console.log(Date.now() - t);
+	t=Date.now();
+	return testFile;
+})
+.then(compile)
+.then(function () {
+	console.log(Date.now() - t);
+	t=Date.now();
+	return testFile;
+})
+.then(compile)
+.then(function () {
+	console.log(Date.now() - t);
+	t=Date.now();
+	return testFile;
+})
+.catch(log('Error:'));
--- test/testFile.js
+++ test/testFile.js
@@ -1,1 +1,3 @@
 console.log('bloop');
+var uniq = require('uniq');
+console.log(uniq([1,1,1,1,2,3,4,4,5,5]));
